4a3fcfbbee1b149b9a78e94027f3cafd2178f7b4
Chris Lieb <chris.lieb@gmail.com>
2022-08-15 20:00:36 -0400

    Mark executable as requiring elevation

--- build.rs
+++ build.rs
@@ -1,5 +1,33 @@
-use windres::Build;
+#[cfg(target_os = "windows")]
+use winres::WindowsResource;
 
+#[cfg(target_os = "windows")]
 fn main() {
-    Build::new().compile("tray-icon.rc").unwrap();
+    let mut res = WindowsResource::new();
+    set_execution_level(&mut res);
+    res.set_icon("icon-blue.ico");
+    res.compile().unwrap();
 }
+
+#[cfg(test)]
+fn set_execution_level(red: &mut WindowsResource) {
+    res.set_manifest(
+        r#"
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+<trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+        <requestedPrivileges>
+            <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+        </requestedPrivileges>
+    </security>
+</trustInfo>
+</assembly>
+"#,
+    );
+}
+
+#[cfg(not(test))]
+fn set_execution_level(_red: &mut WindowsResource) {}
+
+#[cfg(not(target_os = "windows"))]
+fn main() {}
